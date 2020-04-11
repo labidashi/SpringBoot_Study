bc5fd5e3b7d8ec713477f7870052d79b38e9d783
labidashi <331697635@qq.com>
2020-04-11 17:37:10 +0800

    1

---
+++ src/main/java/com/Springboot/config/JdbaConfig.java
@@ -0,0 +1,30 @@
+package com.Springboot.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@EnableConfigurationProperties(JdbcProperties.class)
+public class JdbaConfig {
+
+//    @Bean
+////    public DataSource dataSource(JdbcProperties jdbcProperties) {
+////        DruidDataSource druidDataSource = new DruidDataSource();
+////        druidDataSource.setUrl(jdbcProperties.getUrl());
+////        druidDataSource.setDriverClassName(jdbcProperties.getDriverClassName());
+////        druidDataSource.setPassword(jdbcProperties.getPassword());
+////        druidDataSource.setUsername(jdbcProperties.getUsername());
+////        return druidDataSource;
+////    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource() {
+        return new DruidDataSource();
+    }
+}
---
+++ src/main/java/com/Springboot/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.Springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+@RestController
+public class HelloController {
+    @Autowired
+    private DataSource dataSource;
+
+
+    @GetMapping("hello")
+    public String hello() {
+        System.out.println(dataSource);
+        return "hello";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "hello";
+    }
+}
