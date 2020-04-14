277de34729ebb20301915b45aa55b2cf6dac2c03
labidashi <331697635@qq.com>
2020-04-14 18:01:10 +0800

    整合mybatis: 添加启动器依赖；               2. 配置Mybatis：实体类别名包，日志，映射文件等；               3. 配置MapperScan认hikar

--- src/main/java/com/Springboot/Application.java
+++ src/main/java/com/Springboot/Application.java
@@ -1,10 +1,13 @@
 package com.Springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+//扫描mybatis所有业务mapper接口
+@MapperScan("com.Springboot.mapper")
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/Springboot/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.Springboot.mapper;
+
+public class UserMapper {
+}
