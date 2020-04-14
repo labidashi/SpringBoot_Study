d164a2aa6f43c4d06179e7bb88e3fb9ed3fb7d26
labidashi <331697635@qq.com>
2020-04-14 16:42:04 +0800

    添加mvc拦截器

---
+++ src/main/java/com/Springboot/entity/Student.java
@@ -0,0 +1,12 @@
+package com.Springboot.entity;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+public class Student {
+
+    private String name;
+    private int age;
+}
